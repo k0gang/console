958ac8a0a8f9ed0f992cbaa69078b40069e450fa
k0gang <k0gang@naver.com>
2024-03-29 11:07:12 +0900

    Add : id, pw getter

--- console/src/console/User.java
+++ console/src/console/User.java
@@ -1,10 +1,23 @@
 package console;
 
+import java.util.ArrayList;
+
 public class User {
 	private String id,pw;
 	
+	ArrayList<Post> list = new ArrayList<>();
+	
 	public User(String id, String pw) {
-		
+		this.id = id;
+		this.pw = pw;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getPw() {
+		return this.pw;
 	}
 	
 }
